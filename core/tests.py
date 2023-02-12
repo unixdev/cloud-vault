ecafeae9f6d7ce461d6bb3aa44a069f9864bbf22
Mojahedul Hoque Abul Hasanat <masum@dsinnovators.com>
2023-02-12 23:46:12 +0600

    Add some basic tests
    One test - on short phone number fails.

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from core.forms import SignupForm
+
+
+class LandingViewTest(TestCase):
+    def test_landing_page_renders(self):
+        response = self.client.get('/')
+        self.assertIs(response.status_code, 200)
+
+    def test_landing_page_has_urls(self):
+        response = self.client.get('/')
+        self.assertIn('signup_url', response.context)
+        self.assertIn('login_url', response.context)
+
+
+class SignupViewTest(TestCase):
+    def test_signup_form_appears(self):
+        response = self.client.get(reverse('core:signup'))
+        self.assertIs(response.status_code, 200)
+
+
+class SignupFormTest(TestCase):
+    def test_errors_on_alpha_phone(self):
+        form = SignupForm(data={
+            'phone': 'abcd'
+        })
+        self.assertIn('phone', form.errors)
+
+    def test_errors_on_short_phone(self):
+        form = SignupForm(data={
+            'phone': '0171'
+        })
+        self.assertIn('phone', form.errors)
+
+    def test_valid_phone(self):
+        form = SignupForm(data={
+            'phone': '01712345678'
+        })
+        self.assertNotIn('phone', form.errors)
